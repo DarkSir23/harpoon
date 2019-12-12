416cdb064e1b1da528a044da0349f85ce1264ac9
DarkSir23 <jason@fortsackville.com>
2019-12-12 10:54:38 -0500

    Tiny Fixes

--- harpoon/watcher.py
+++ harpoon/watcher.py
@@ -8,18 +8,18 @@ class Watcher:
     def __init__(self, scanner):
         self.observer = Observer()
         self.scanner = scanner
-        self.event_handler = PatternMatchingEventHandler(case_sensitive=False, patterns=["*.torrent", "*.nzb", "*.file"])
-        self.event_handler.on_created = self.on_created
+        self.event_handler = PatternMatchingEventHandler(case_sensitive=False, patterns=["*.torrent", "*.nzb", "*.file", "*.hash"])
+        self.event_handler.on_any_event = self.on_any_event
         self.observer.schedule(self.event_handler, config.GENERAL['torrentfile_dir'], recursive=True)
         logger.debug('SCANNER: Monitoring %s' % config.GENERAL['torrentfile_dir'])
         self.observer.start()
 
-    def on_created(self, event):
+    def on_any_event(self, event):
         logger.debug('SCANNER: Event.')
         if event.is_directory:
             return None
         else:
-            logger.debug('SCANNER: New file detected.  Running scanner.')
+            logger.debug('SCANNER: File change detected.  Running scanner.')
             self.scanner.scan()
 
     def stop(self):
